ea93d237c795200b7b46f0a92ccc7d3a02fee0bd
Avula Someshwara Kartheek <karthikavula.contact@gmail.com>
2026-03-09 01:07:37 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 
 function login(){
-const API_URL = "https://script.google.com/macros/s/AKfycbyNeFEI36Z3DvERgAP4BZnGFPfG3J7lKEfE4WcwpVbOl4Kb4lMnbpC-LSN5YTejragJ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxfSTHxRUlF6v8-yzTMjF_aQn2698RQsfZX-jKQOZ21ryTKLzsbDwZzhuGXS0LMDEa3/exec";
 const email =
 document.getElementById("email").value;
 
