8936d353ab8be9377961732cbad0d4fa52b64cbf
Avula Someshwara Kartheek <karthikavula.contact@gmail.com>
2026-03-08 23:17:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+if(document.getElementById("reader")){
+
+let lastScan = null;
+
 const scanner = new Html5Qrcode("reader");
 
 scanner.start(
@@ -6,9 +12,77 @@ scanner.start(
 
 function(decodedText){
 
+if(decodedText === lastScan){
+return;
+}
+
+lastScan = decodedText;
+
 document.getElementById("result").innerText =
-"Scanned Ticket: " + decodedText;
+"Verifying Ticket...";
+
+/* SEND QR TO APPS SCRIPT */
+
+fetch("https://script.google.com/macros/s/AKfycbw1rsZj15qsa53SzQpN0XPPIe3LZ2d30vLhfLhZ5zb3YAUBjAq4qwnh08f5afTaRUCg/exec", {
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+qrText:decodedText,
+day:"Day-1",
+event:"Registration Desk",
+volunteer:"Scanner Device"
+
+})
+
+})
+
+.then(res => res.json())
+.then(data => {
+
+if(data.status === "success"){
+
+document.getElementById("result").innerText =
+"ENTRY CONFIRMED: " + data.name;
+
+}
+
+else if(data.status === "duplicate"){
+
+document.getElementById("result").innerText =
+"ALREADY SCANNED";
+
+}
+
+else{
+
+document.getElementById("result").innerText =
+"INVALID TICKET";
+
+}
+
+setTimeout(()=>{
+lastScan = null;
+},2000);
+
+})
+
+.catch(err => {
+
+document.getElementById("result").innerText =
+"SERVER ERROR";
+
+});
 
 }
 
 );
+
+}
+
+});
